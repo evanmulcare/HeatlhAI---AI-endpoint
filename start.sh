6adc55b7e1b964d7226014e9b0510d20dabeb3d5
Evan Mulcare R00211686 <evanmulcare@gmail.com>
2023-11-11 12:35:08 +0000

    testnew

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+# Activate the virtual environment
 source env/bin/activate
 
-# Run your Flask app
-gunicorn app:app
+# Run your Flask app using Gunicorn
+gunicorn -w 4 -b 0.0.0.0:5000 app:app  # Update with your actual file name
+
+# Deactivate the virtual environment (optional, depending on your requirements)
+deactivate
